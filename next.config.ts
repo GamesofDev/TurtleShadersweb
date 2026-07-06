2a28c03a3f93bf9d1ac2c1e978f6b71ccfb22078
GamesofDev <gamesofdevappsuse@gmail.com>
2026-07-06 19:18:16 +0530

    ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     unoptimized: true,
   },
-}
+  basePath: "/TurtleShadersweb",
+  assetPrefix: "/TurtleShadersweb/",
+  trailingSlash: true,
+};
 
-export default nextConfig
+export default nextConfig;
